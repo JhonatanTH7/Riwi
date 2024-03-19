a40850296833a78a3ee3f715f946a3e7df5828a5
JhonatanTH7 <Jhonatantorohurtado93@gmail.com>
2024-03-19 08:38:45 -0500

    Update DB

--- Java/DBConnections/JDBC/src/Introduction/Main.java
+++ Java/DBConnections/JDBC/src/Introduction/Main.java
@@ -1,9 +1,49 @@
 package Introduction;
 
+import Introduction.controller.CoderController;
 import Introduction.database.ConfigDB;
+
+import javax.swing.*;
 
 public class Main {
     public static void main(String[] args) {
-        ConfigDB.openConnection();
+        CoderController objCoderController = new CoderController();
+        String option = "";
+        do {
+            option = JOptionPane.showInputDialog(null, """
+                                        
+                    MENU
+                                        
+                    1. Show all Coders
+                    2. Add Coder
+                    3. Update Coder
+                    4. Delete Coder
+                    5. Get Coder by name
+                    6. Exit
+                                        
+                    Choose an option:
+                                        
+                    """);
+            switch (option) {
+                case "1":
+                    //Show Coders
+                    objCoderController.getAll();
+                    break;
+                case "2":
+                    break;
+                case "3":
+                    break;
+                case "4":
+                    break;
+                case "5":
+                    break;
+                case "6":
+                    JOptionPane.showMessageDialog(null, "See you next time!!!");
+                    break;
+                default:
+                    JOptionPane.showMessageDialog(null, "Not a valid option, try again");
+                    break;
+            }
+        } while (!option.equals("6"));
     }
 }
--- Java/DBConnections/JDBC/src/Introduction/controller/CoderController.java
+++ Java/DBConnections/JDBC/src/Introduction/controller/CoderController.java
@@ -1,14 +1,31 @@
 package Introduction.controller;
 
+import Introduction.entity.Coder;
 import Introduction.model.CoderModel;
+
+import javax.swing.*;
 
 public class CoderController {
     CoderModel objCoderModel;
 
-    CoderController() {
+    public CoderController() {
+        //Crear una instancia del Model
         this.objCoderModel = new CoderModel();
     }
 
+    //Método para listar los elementos (Coders)
     public void getAll() {
+        //Definimos String para concatenar la info
+        String list = "                             ==== Coders List ==== \n";
+        //Iteramos sobre la lista que devuelve el método find all
+        for (Object obj : this.objCoderModel.findAll()) {
+            //Casteamos el tipo Object a tipo Coder
+            Coder objCoder = (Coder) obj;
+            //Concatenamos la info
+            list += "- ID: " + objCoder.getId() + " Name: " + objCoder.getName() + " Age: " + objCoder.getAge() + " Clan: " + objCoder.getClan() + "\n";
+        }
+
+        //Imprimimos la lista
+        JOptionPane.showMessageDialog(null, list);
     }
 }
